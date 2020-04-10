69e9447866bbd041b6328f7424ea0f8c0e20f41c
Ivan Blinkov <github@blinkov.ru>
2020-04-10 21:16:58 +0300

    adjust csp

--- src/util.ts
+++ src/util.ts
@@ -6,7 +6,7 @@ export function addDefaultHeaders(response: Response) {
   response.headers.set('x-xss-protection', '1; mode=block');
   response.headers.set(
     'content-security-policy',
-    `default-src 'none'; script-src 'self' 'unsafe-eval' ajax.cloudflare.com static.cloudflareinsights.com mc.yandex.ru; style-src 'self'; img-src 'self' data: mc.yandex.ru; connect-src 'self' mc.yandex.ru; child-src blob: mc.yandex.ru; frame-src blob: mc.yandex.ru; font-src 'self' data:;`,
+    `default-src 'none'; script-src 'self' 'unsafe-eval' ajax.cloudflare.com static.cloudflareinsights.com *.algolia.net *.algolianet.com mc.yandex.ru; style-src 'self'; img-src 'self' data: mc.yandex.ru; connect-src 'self' mc.yandex.ru *.algolia.net *.algolianet.com; child-src blob: mc.yandex.ru; frame-src blob: mc.yandex.ru; font-src 'self' data:;`,
   );
   let location = response.headers.get('location');
   if (location && location.indexOf(config.origin) >= 0) {
