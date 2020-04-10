517322e17e66ed9f72ada0446a4d2b06b63f9e94
Ivan Blinkov <github@blinkov.ru>
2020-04-10 20:40:01 +0300

    Convert client-side redirects to server-side redirects

--- src/handler.ts
+++ src/handler.ts
@@ -9,6 +9,22 @@ export async function handleRequest(request: Request): Promise<Response> {
   }
   url.hostname = config.origin;
   let response = await fetch(changeUrl(request, url));
+  if (response.status === 200 && url.pathname.startsWith('/docs') && !url.pathname.endsWith('.js')) {
+    let text = await response.text();
+    let redirect_prefix = '<!-- Redirect: ';
+    if (text.startsWith(redirect_prefix)) {
+      let headers = new Headers();
+      headers.set('location', text.substring(redirect_prefix.length).split(' -->', 1)[0]);
+      return new Response('301 Moved Permanently', {
+        status: 301,
+        headers: headers
+      })
+    } else {
+      response = new Response(text, response);
+      addDefaultHeaders(response);
+      return response;
+    }
+  }
   response = new Response(response.body, response);
   addDefaultHeaders(response);
   return response;
