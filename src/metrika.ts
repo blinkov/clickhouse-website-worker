a7fb7b67a0150092e3446479848c4107884d1130
Ivan Blinkov <github@blinkov.ru>
2020-04-18 00:11:58 +0300

    adjust headers

--- src/metrika.ts
+++ src/metrika.ts
@@ -3,5 +3,11 @@ import { addDefaultHeaders, changeUrl } from './util';
 export async function handleMetrikaCounterRequest(request: Request): Promise<Response> {
   let url = new URL('https://mc.yandex.ru/metrika/tag.js');
   let cf = { cf: { cacheEverything: true, cacheTtl: 86400, minify: { javascript: true, css: false, html: false }}};
-  return fetch(changeUrl(request, url), cf);
+  let response = await fetch(changeUrl(request, url), cf);
+  response = new Response(response.body, response)
+  addDefaultHeaders(response);
+  response.headers.set('cache-control', 'public, max-age=259200');
+  response.headers.delete('expires');
+  return response;
+
 }
