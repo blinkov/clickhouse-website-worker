40acd28bdb0b92eb1a0795d8ebfa4f6930120c2a
Ivan Blinkov <github@blinkov.ru>
2020-05-08 13:31:13 +0300

    Adjust CSP

--- src/util.ts
+++ src/util.ts
@@ -21,7 +21,7 @@ export function addDefaultHeaders(response: Response) {
   response.headers.set('x-xss-protection', '1; mode=block');
   response.headers.set(
     'content-security-policy',
-    `default-src 'none'; script-src 'self' 'unsafe-eval' ajax.cloudflare.com static.cloudflareinsights.com *.algolia.net *.algolianet.com mc.yandex.ru; style-src 'self' 'unsafe-inline'; img-src 'self' data: mc.yandex.ru; connect-src 'self' mc.yandex.ru *.algolia.net *.algolianet.com; child-src blob: mc.yandex.ru; frame-src blob: mc.yandex.ru; font-src 'self' data:; base-uri 'none'; form-action 'self'; frame-ancestors 'none'; prefetch-src 'self'`,
+    `default-src 'none'; script-src 'self' 'unsafe-eval' cdn.ampproject.org ajax.cloudflare.com static.cloudflareinsights.com *.algolia.net *.algolianet.com mc.yandex.ru; style-src 'self' 'unsafe-inline'; img-src 'self' data: mc.yandex.ru; connect-src 'self' mc.yandex.ru *.algolia.net *.algolianet.com; child-src blob: mc.yandex.ru; frame-src blob: mc.yandex.ru; font-src 'self' data:; base-uri 'none'; form-action 'self'; frame-ancestors 'none'; prefetch-src 'self'`,
   );
   const location = response.headers.get('location');
   if (location && location.indexOf(config.origin) >= 0) {
