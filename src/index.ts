a0544960edfbc71ad6c423ef5a427f9b46156517
Ivan Blinkov <github@blinkov.ru>
2020-05-22 13:53:56 +0300

    [experimental] cache worker results

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,15 @@ addEventListener('fetch', (event) => {
 
 async function handleEvent(event: FetchEvent) {
   try {
-    return await handleRequest(event.request);
+    let response;
+    if (event.request.method.toLowerCase() == 'get') {
+      response = await caches.default.match(event.request);
+    }
+    if (!response) {
+      response = await handleRequest(event.request);
+      cacheResponse(event, response);
+    }
+    return response;
   } catch (e) {
     event.waitUntil(sendExceptionToSentry(e, event.request));
     return fallbackResponse(event.request);
@@ -20,4 +28,10 @@ async function fallbackResponse(request: Request): Promise<Response> {
   let url = new URL(request.url);
   url.hostname = config.origin;
   return await fetch(changeUrl(request, url));
+}
+
+function cacheResponse(event: FetchEvent, response: Response) {
+  const to_cache = response.clone();
+  to_cache.headers.set('cache-control', 'public, max-age=43200');
+  event.waitUntil(caches.default.put(event.request, to_cache));
 }
